2f15cb8a5d3f8379368bfc4b06c3e09a10a12529
Steve Marx <Steve.Marx@microsoft.com>
2011-09-01 17:26:26 -0700

    initial commit

---
+++ ExistenceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.StorageClient;
+
+namespace smarx.WazStorageExtensions
+{
+    public static class ExistenceExtensions
+    {
+        public static bool Exists(this CloudBlob blob)
+        {
+            try
+            {
+                blob.FetchAttributes();
+                return true;
+            }
+            catch (StorageClientException e)
+            {
+                if (e.ErrorCode == StorageErrorCode.ResourceNotFound)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        public static bool Exists(this CloudBlobContainer container)
+        {
+            try
+            {
+                container.FetchAttributes();
+                return true;
+            }
+            catch (StorageClientException e)
+            {
+                if (e.ErrorCode == StorageErrorCode.ResourceNotFound)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
