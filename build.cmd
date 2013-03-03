40f12a7bbe2dee3e45a8b0fd556c9bc89dbdbfe7
Harry Pierson <harry@devhawk.net>
2013-03-03 14:17:23 -0800

    Updated to latest version of nuget and cleaned up build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,4 @@
 for %%f in (*.csproj) do (
     msbuild %%f /p:"configuration=release"
-    .nuget\NuGet.exe pack %%f -Prop Configuration=Release
     .nuget\NuGet.exe pack %%f -Prop Configuration=Release -Symbols
 )
