e24a4607e95ae885ef457ea96f356767bf0834d2
Harry Pierson <harry@devhawk.net>
2013-03-04 00:16:07 -0800

    Removed sync AutoRenewLease constructor
    commented out DoOnce/DoEvery until I can updated them to be async

--- WazStorageExtensions/AutoRenewLease.cs
+++ WazStorageExtensions/AutoRenewLease.cs
@@ -18,90 +18,62 @@ namespace smarx.WazStorageExtensions
         private Thread renewalThread;
         private bool disposed = false;
 
-        public static void DoOnce(ICloudBlob blob, Action action) { DoOnce(blob, action, TimeSpan.FromSeconds(5)); }
-        public static void DoOnce(ICloudBlob blob, Action action, TimeSpan pollingFrequency)
-        {
-            // blob.Exists has the side effect of calling blob.FetchAttributes, which populates the metadata collection
-            while (!blob.Exists() || blob.Metadata["progress"] != "done")
-            {
-                using (var arl = new AutoRenewLease(blob))
-                {
-                    if (arl.HasLease)
-                    {
-                        action();
-                        blob.Metadata["progress"] = "done";
-                        blob.SetMetadata(AccessCondition.GenerateLeaseCondition(arl.leaseId));
-                    }
-                    else
-                    {
-                        Thread.Sleep(pollingFrequency);
-                    }
-                }
-            }
-        }
+        //public static void DoOnce(ICloudBlob blob, Action action) { DoOnce(blob, action, TimeSpan.FromSeconds(5)); }
+        //public static void DoOnce(ICloudBlob blob, Action action, TimeSpan pollingFrequency)
+        //{
+        //    // blob.Exists has the side effect of calling blob.FetchAttributes, which populates the metadata collection
+        //    while (!blob.Exists() || blob.Metadata["progress"] != "done")
+        //    {
+        //        var t = AutoRenewLease.GetAutoRenewLeaseAsync(blob);
+        //        t.Wait();
 
-        public static void DoEvery(ICloudBlob blob, TimeSpan interval, Action action)
-        {
-            while (true)
-            {
-                var lastPerformed = DateTimeOffset.MinValue;
-                using (var arl = new AutoRenewLease(blob))
-                {
-                    if (arl.HasLease)
-                    {
-                        blob.FetchAttributes();
-                        DateTimeOffset.TryParseExact(blob.Metadata["lastPerformed"], "R", CultureInfo.CurrentCulture, DateTimeStyles.AdjustToUniversal, out lastPerformed);
-                        if (DateTimeOffset.UtcNow >= lastPerformed + interval)
-                        {
-                            action();
-                            lastPerformed = DateTimeOffset.UtcNow;
-                            blob.Metadata["lastPerformed"] = lastPerformed.ToString("R");
-                            blob.SetMetadata(AccessCondition.GenerateLeaseCondition(arl.leaseId));
-                        }
-                    }
-                }
-                var timeLeft = (lastPerformed + interval) - DateTimeOffset.UtcNow;
-                var minimum = TimeSpan.FromSeconds(5); // so we're not polling the leased blob too fast
-                Thread.Sleep(
-                    timeLeft > minimum
-                    ? timeLeft
-                    : minimum);
-            }
-        }
+        //        using (var arl = t.Result)
+        //        {
+        //            if (arl.HasLease)
+        //            {
+        //                action();
+        //                blob.Metadata["progress"] = "done";
+        //                blob.SetMetadata(AccessCondition.GenerateLeaseCondition(arl.leaseId));
+        //            }
+        //            else
+        //            {
+        //                Thread.Sleep(pollingFrequency);
+        //            }
+        //        }
+        //    }
+        //}
 
-        public AutoRenewLease(ICloudBlob blob)
-        {
-            this.blob = blob;
-            blob.Container.CreateIfNotExists();
-            try
-            {
-                using (var ms = new System.IO.MemoryStream(new byte[0]))
-                {
-                    blob.UploadFromStream(ms, AccessCondition.GenerateIfNoneMatchCondition("*"));
-                }
-            }
-            catch (StorageException e)
-            {
-                if (e.RequestInformation.HttpStatusCode != (int)HttpStatusCode.Conflict &&         // 409 from trying to modify a blob that already exists
-                    e.RequestInformation.HttpStatusCode != (int)HttpStatusCode.PreconditionFailed) // 412 from trying to modify a blob that's leased
-                {
-                    throw;
-                }
-            }
-            leaseId = blob.TryAcquireLease();
-            if (HasLease)
-            {
-                renewalThread = new Thread(() =>
-                {
-                    while (true)
-                    {
-                        Thread.Sleep(TimeSpan.FromSeconds(40));
-                        blob.RenewLease(AccessCondition.GenerateLeaseCondition(leaseId));
-                    }
-                });
-                renewalThread.Start();
-            }
-        }
+        //public static void DoEvery(ICloudBlob blob, TimeSpan interval, Action action)
+        //{
+        //    while (true)
+        //    {
+        //        var lastPerformed = DateTimeOffset.MinValue;
+        //        var t = AutoRenewLease.GetAutoRenewLeaseAsync(blob);
+        //        t.Wait();
+
+        //        using (var arl = t.Result)
+        //        {
+        //            if (arl.HasLease)
+        //            {
+        //                blob.FetchAttributes();
+        //                DateTimeOffset.TryParseExact(blob.Metadata["lastPerformed"], "R", CultureInfo.CurrentCulture, DateTimeStyles.AdjustToUniversal, out lastPerformed);
+        //                if (DateTimeOffset.UtcNow >= lastPerformed + interval)
+        //                {
+        //                    action();
+        //                    lastPerformed = DateTimeOffset.UtcNow;
+        //                    blob.Metadata["lastPerformed"] = lastPerformed.ToString("R");
+        //                    blob.SetMetadata(AccessCondition.GenerateLeaseCondition(arl.leaseId));
+        //                }
+        //            }
+        //        }
+        //        var timeLeft = (lastPerformed + interval) - DateTimeOffset.UtcNow;
+        //        var minimum = TimeSpan.FromSeconds(5); // so we're not polling the leased blob too fast
+        //        Thread.Sleep(
+        //            timeLeft > minimum
+        //            ? timeLeft
+        //            : minimum);
+        //    }
+        //}
 
         private AutoRenewLease(ICloudBlob blob, string leaseId, Thread renewalThread)
         {
@@ -136,7 +108,7 @@ namespace smarx.WazStorageExtensions
                 }
             }
 
-            var leaseId = await blob.TryAquireLeaseAsync();
+            var leaseId = await blob.TryAquireLeaseAsync(TimeSpan.FromSeconds(60));
             Thread renewalThread = null;
 
             if (leaseId != null)
